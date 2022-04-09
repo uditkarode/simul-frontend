cf388d439e5912d491e6451685baa81cec334e1a
Udit Karode <udit.karode@gmail.com>
2022-04-09 20:51:16 +0530

    feat: add react-location

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/Main.css";
 import Landing from "./pages/Landing";
+import { ReactLocation, Router } from "@tanstack/react-location";
+
+const location = new ReactLocation();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing />
+    <Router location={location} routes={[
+      { path: "/", element: <Landing /> }
+    ]} />
   </React.StrictMode>,
   document.getElementById("root")
 );
