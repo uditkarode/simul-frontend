2bc935afebf44749a7e627bcd94812fe8ef406af
Udit Karode <udit.karode@gmail.com>
2022-04-10 21:44:35 +0530

    feat(Landing): initial UI

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,29 @@
 import "../styles/Landing.css";
 import { ReactComponent as Menu } from "../../assets/svg/menu.svg";
+import { css } from "@emotion/css";
 
 function Landing() {
+  const centreContent = css`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const fun = css`
+    font-weight: 700;
+    color: #502c56;
+  `;
+
   return (
     <>
-      <Menu />
+      <Menu style={{ marginTop: 31, marginLeft: 36 }} />
+      <div className={centreContent}>
+        <p style={{ fontSize: "4rem", fontWeight: 500 }}>
+          Join in on the <span className={fun}>fun</span>!
+        </p>
+      </div>
     </>
   );
 }
